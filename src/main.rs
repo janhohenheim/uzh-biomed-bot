8881f9392633b3784d5e9f509bb8a662ae436a5e
Jan Nils Ferner <contact@jnferner.com>
2020-09-21 10:16:44 +0200

    Implement basic tbot scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+use std::env;
+use tbot::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().expect("Failed to read .env");
+    let mut bot = tbot::Bot::from_env("BOT_TOKEN").event_loop();
 }
