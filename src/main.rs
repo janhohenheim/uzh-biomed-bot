835a6e37834674c361b40a31531165f6898de302
Jan Nils Ferner <contact@jnferner.com>
2020-09-21 10:20:22 +0200

    Implement echoing

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,13 @@ use tbot::prelude::*;
 async fn main() {
     dotenv::dotenv().expect("Failed to read .env");
     let mut bot = tbot::Bot::from_env("BOT_TOKEN").event_loop();
+    bot.text(|context| async move {
+        let message = format!("You sent me {}", context.text.value);
+        context
+            .send_message_in_reply(&message)
+            .call()
+            .await
+            .unwrap();
+    });
+    bot.polling().start().await.unwrap();
 }
