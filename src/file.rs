a1750a4086ca603e4982f3f385b93f10f46f5c6d
Jan Nils Ferner <contact@jnferner.com>
2020-09-25 22:08:02 +0200

    Actually search module

--- src/file.rs
+++ src/file.rs
@@ -28,11 +28,13 @@ fn open_settings_file() -> Result<File, std::io::Error> {
         .open(constant::SETTINGS_FILE)
 }
 
-fn read_module(identifier: &str) -> Result<Module, std::io::Error> {
-    let file = open_modules_file()?;
+fn read_module(identifier: &str) -> Result<Option<Module>, Box<dyn Error>> {
+    let mut file = open_modules_file()?;
     let mut toml = String::new();
     file.read_to_string(&mut toml)?;
     let modules: Vec<Module> = toml::from_str(&toml).unwrap_or_default();
+
+    Ok(modules.into_iter().find(|module| module.identifier == identifier))
 }
 
 fn open_modules_file() -> Result<File, std::io::Error> {
