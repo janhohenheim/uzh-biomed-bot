9d0f3467c1f837fe961ab95af1600b75eeda588e
Jan Nils Ferner <contact@jnferner.com>
2020-10-14 12:54:36 +0200

    Add keyboards

--- src/constant.rs
+++ src/constant.rs
@@ -1,2 +1,5 @@
 pub const SETTINGS_FILE: &'static str = "chats.json";
 pub const MODULES_FILE: &'static str = "modules.json";
+
+pub const MATHS_CALLBACK: &'static str = "MAT 182";
+pub const PHYSICS_CALLBACK: &'static str = "PHY 117";
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![feature(type_alias_impl_trait)]
 
 use uzh_biomed_bot::chat::*;
+use uzh_biomed_bot::constant;
 use uzh_biomed_bot::persistence::*;
 use uzh_biomed_bot::scheduling::*;
 
 use dotenv;
 use std::error::Error;
-use tbot::prelude::*;
-
+use tbot::{
+    prelude::*,
+    types::keyboard::inline::{Button, ButtonKind},
+};
 type Context<T> = std::sync::Arc<tbot::contexts::Command<tbot::contexts::Text<T>>>;
+type CallbackContext<T> = std::sync::Arc<tbot::contexts::DataCallback<T>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -23,6 +27,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     bot.start(handle_subscription);
     bot.command("subscribe", handle_subscription);
     bot.command("unsubscribe", handle_unsubscription);
+    bot.command("links", handle_links);
+    bot.data_callback(handle_callback);
 
     bot.polling().start().await.unwrap();
     Ok(())
@@ -79,5 +85,38 @@ async fn handle_unsubscription(context: Context<impl tbot::connectors::Connector
                 .call()
                 .await
                 .unwrap();
+    }
+}
+
+async fn handle_links(context: Context<impl tbot::connectors::Connector>) {
+    const KEYBOARD: &[&[Button]] = &[&[
+        Button::new(
+            "MAT 182",
+            ButtonKind::CallbackData(constant::MATHS_CALLBACK),
+        ),
+        Button::new(
+            "PHY 117",
+            ButtonKind::CallbackData(constant::PHYSICS_CALLBACK),
+        ),
+    ]];
+
+    context
+        .send_message("Select the module you wish to see links for")
+        .reply_markup(KEYBOARD)
+        .call()
+        .await
+        .unwrap();
+}
+
+async fn handle_callback(context: CallbackContext<impl tbot::connectors::Connector>) {
+    let message = match context.data.as_str() {
+        constant::MATHS_CALLBACK => "You're cool too!",
+        constant::PHYSICS_CALLBACK => "Thanks, I'm trying!",
+        _ => "Are you trying to hack me?",
+    };
+
+    let call_result = context.notify(message).call().await;
+    if let Err(err) = call_result {
+        dbg!(err);
     }
 }
